2447a7d6eb938c4f18d29853a43a5686aade0c2b
alexhills.la@gmail.com <aleks@Aleksandrs-MacBook-Pro.local>
2022-05-24 21:34:51 +0300

    add new files

---
+++ Start_1.java
@@ -0,0 +1,21 @@
+public class Start_1 {
+    public static void main (String[] args) {
+
+        int num1 = 7;
+        int num2 = 6;
+        int num3 = 9;
+        int num4 = 5;
+
+        int result = num1 + num2;
+
+        if (num1 == num2 || num3 > num4) {
+            System.out.println("Result is done ;)");
+        } else {
+            System.out.println("Result is not done :(");
+        }
+
+
+
+
+    }
+}
